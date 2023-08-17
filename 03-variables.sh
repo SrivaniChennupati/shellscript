b17e2f0cac276aad686a4d9f60f1e4cce71b6f9a
Srivani <SrivaniChennupati@gmail.com>
2023-08-17 16:42:30 -0500

    read command

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Take input from user and calculate sum.
-read -p "Enter Fisrt Number" : a
+read -p "Enter Fisrt Number": a
 read -p "Enter Second Number": b
 sum=$(($a+$b))
 echo "sum of the 2 given numbers is $sum"
