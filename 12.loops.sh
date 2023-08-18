fd789093fbbd51704cf46ddcd5be30eef062c50f
Srivani <SrivaniChennupati@gmail.com>
2023-08-17 22:53:18 -0500

    installingmysql

--- 12.loops.sh
+++ 12.loops.sh
@@ -10,9 +10,10 @@ validate(){
 
 if [ $1 -ne 0 ]
 then
-        echo "$2.....SUCCESS"
+        echo "$2.....FAILURE"
+        exit 1
 else
-        echo "$2......FAILURE"
+        echo "$2......SUCCESS"
 fi        
 
 }
