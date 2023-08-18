20bb9afe0c032ee51b9a699a1aab18097799e3de
Srivani <SrivaniChennupati@gmail.com>
2023-08-17 19:07:26 -0500

    conditions

--- 08-Conditions.sh
+++ 08-Conditions.sh
@@ -3,7 +3,7 @@ number=$1
 
 if[ $number -gt 10 ] 
 then
-echo "Given number $number is Greater than 10"
+    echo "Given number $number is Greater than 10"
 else
-echo "Given number $number is not greater than 10"
+    echo "Given number $number is not greater than 10"
 fi
