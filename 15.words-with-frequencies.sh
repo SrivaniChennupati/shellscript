c90d38deb982e114d567ed71db43b02a07476295
Srivani <SrivaniChennupati@gmail.com>
2023-08-20 19:40:14 -0500

    Words-with-frequencies

--- 15.words-with-frequencies.sh
+++ 15.words-with-frequencies.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 Text_File_Path=/home/centos/shellscript/Testfile
-#sort command sorts the contents of the text file line by line - Sorts in ASCII
-#uniq Identify and deletes the adjucent dupliacte lines from the input file , it should use with sort , -c will helps how many times a line was repeated by displaying number as a prefix with the line 
-#uniq -c to count the frequency of each word
-#sort -nr to sort the word frequency list in descending order by frequency
+
+# checks if the input file exists.
+#tokenizes the text into words using tr.
+#sorts the words.
+#counts the frequency of each word using uniq -c.
+#sorts the word-frequency pairs in descending order using sort -nr.
+#prints the sorted word frequency list.
 
 word_frequency=$(cat $Text_File_Path | tr -s ' ' '\n' | sort | uniq -c | sort -nr | awk '{print $2"@"$1}')
 
-echo "Word Frequency List : "
+echo "Word Frequency List :"
 echo $word_frequency
