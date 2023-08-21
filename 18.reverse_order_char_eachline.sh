ed58b3a9f875fb670b5ef88bee6dc3b1f9037467
Srivani <SrivaniChennupati@gmail.com>
2023-08-20 21:09:31 -0500

    palindrome

--- 18.reverse_order_char_eachline.sh
+++ 18.reverse_order_char_eachline.sh
@@ -6,7 +6,7 @@ output_file=/home/centos/shellscript/output_reverseorder
 
 while read line
 do  
-    Reversed_line= $(echo $line | rev)
+    Reversed_line=$(echo "$line" | rev)
    echo "Reversed line :" $Reversed_line > $output_file
 done < $input_file   
   
