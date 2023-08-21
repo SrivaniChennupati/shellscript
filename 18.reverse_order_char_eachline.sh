61163a9046c7a55d13b82d3668b5d6781cfe3e7c
Srivani <SrivaniChennupati@gmail.com>
2023-08-20 21:12:59 -0500

    reverse-order

--- 18.reverse_order_char_eachline.sh
+++ 18.reverse_order_char_eachline.sh
@@ -4,9 +4,9 @@ input_file=/home/centos/shellscript/reverseorder
 
 output_file=/home/centos/shellscript/output_reverseorder
 
-while read line
+while read -r line
 do  
     Reversed_line=$(echo "$line" | rev)
    echo "Reversed line :" $Reversed_line > $output_file
-done <<< $input_file   
+done < $input_file   
   
