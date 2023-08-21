fd45ce0e99b46eb01a7d687970543a0f87c1c642
Srivani <SrivaniChennupati@gmail.com>
2023-08-20 19:52:56 -0500

    Words-with-frequencies

--- 15.words-with-frequencies.sh
+++ 15.words-with-frequencies.sh
@@ -9,6 +9,13 @@ Text_File_Path=/home/centos/shellscript/Testfile
 #sorts the word-frequency pairs in descending order using sort -nr.
 #prints the sorted word frequency list.
 
+if [ ! -f $Text_File_Path ]
+then
+    echo "File not Found : " $Text_File_Path
+    exit 1
+fi    
+   
+
 word_frequency=$(cat $Text_File_Path | tr -s ' ' '\n' | sort | uniq -c | sort -nr | awk '{print $2"@"$1}')
 
 echo "Word Frequency List :"
