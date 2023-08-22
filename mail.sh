6ac2708352ff33e08326f00655b66b5fb30a6560
Srivani <SrivaniChennupati@gmail.com>
2023-08-21 21:34:30 -0500

    email

--- mail.sh
+++ mail.sh
@@ -2,11 +2,11 @@
 
 #Anyone in our project can call this script with Arguements
 
-TO_ADDRESS=$1
-SUBJECT=$2
-BODY=$3
-TEAM_NAME=$4
-ALERT_TYPE=$5
+TO_ADDRESS="$1"
+SUBJECT="$2"
+BODY="$3"
+TEAM_NAME="$4"
+ALERT_TYPE="$5"
 
 
 #echo " all args :" $@
