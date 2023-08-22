ca47f3d76bc7d7b356c4ac5ee8b5cc89ee7a432a
Srivani <SrivaniChennupati@gmail.com>
2023-08-21 21:21:20 -0500

    email

--- 19.disk_usage.sh
+++ 19.disk_usage.sh
@@ -40,5 +40,5 @@ echo "$message $(date)" | mail -s "High Disk Usage" srivanichennupati@gmail.com
 
 #sh mail.sh srivanichennupati@gmail.com "High Disk Usage" $message "Devops Team" "High Disk Usage"
 
-sh mail.sh srivanichennupati@gmail.com "High Disk Usage" $message "Devops Team" "High Disk Usage"
+#sh mail.sh srivanichennupati@gmail.com "High Disk Usage" $message "Devops Team" "High Disk Usage"
 
