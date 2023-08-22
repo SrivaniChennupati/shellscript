6d8bc2fee9d3ae7f282c402e56f141f3b74cbf55
Srivani <SrivaniChennupati@gmail.com>
2023-08-21 20:49:36 -0500

    Disk_usage

--- mail.sh
+++ mail.sh
@@ -12,9 +12,8 @@ ALERT_TYPE=$5
 #mail command
 #Body we needto prepare it using template
 
-FINAL_BODY=$(sed -e "s|TEAM_NAME|$TEAM_NAME|g" -e "s|ALERT_TYPE|$ALERT_TYPE|g" -e "s|MESSAGE|$BODY|g" template.html)
+FINAL_BODY=$(sed -e "s|Team_Name|$TEAM_NAME|g" -e "s|Alert_Type|$ALERT_TYPE|g" -e "s|Message|$BODY|g" template.html)
 
 echo $FINAL_BODY
-
 
 echo "$fINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
